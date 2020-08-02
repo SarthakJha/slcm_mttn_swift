76d956db5f11a6d69428c566f15dcd1c63bd8fea
Sarthak Jha <sarthakjha2001@gmail.com>
2020-08-02 16:58:08 +0530

    Initial Commit

---
+++ slcm_MTTN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slcm_MTTN
+//
+//  Created by Sarthak Jha  on 02/08/20.
+//  Copyright © 2020 Sarthak Jha . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
